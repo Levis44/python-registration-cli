803786f52a13d69399246d8bfd730bd6d1ed537c
Levi <levi.ciarrocchi@gmail.com>
2022-10-11 21:16:17 -0300

    chore: first handler of registration

---
+++ handlers.py
@@ -0,0 +1,25 @@
+from client import Client
+import menus
+
+clients = []
+
+def handleRegister():
+  # pick first info
+  client_name, client_birth, client_phone, client_email = menus.initClientRegistration()
+
+  # create client
+  client = Client(client_name, client_birth, client_phone, client_email)
+
+  # choose category
+  category = menus.categoryMenu()
+
+  # add category to client
+  client.categories.append(category)
+
+  # add attributes from category
+  client.categoryQuestions(category)
+
+  # add client to fake database
+  clients.append(client)
+
+  print(vars(client))
